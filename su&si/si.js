812c0d1ce62e567cd981bc30337b41d40380a73f
Khoa Lê Anh <khoaleanh225@gmail.com>
2025-03-23 14:53:30 +0700

    chưa server + API

--- su&si/si.js
+++ su&si/si.js
@@ -1,23 +1,52 @@
-function login(){
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    if (username === "" || password === ""){
-        alert ("Vui lòng nhập đầy đủ thông tin!");
+// Google
+function signinWithGoogle() {
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signIn().then(googleUser => {
+        const profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId());
+        console.log('Name: ' + profile.getName());
+        console.log('Email: ' + profile.getEmail());
+    }).catch(error => {
+        console.error('Google Sign-In Error:', error);
+    });
+}
+
+// Facebook
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: 'YOUR_FACEBOOK_APP_ID',
+        cookie: true,
+        xfbml: true,
+        version: 'v12.0'
+    });
+
+    FB.AppEvents.logPageView();
+};
+
+function signinWithFacebook() {
+    FB.login(response => {
+        if (response.authResponse) {
+            FB.api('/me', { fields: 'name,email' }, function(response) {
+                console.log('Good to see you, ' + response.name + '.');
+                console.log('Email: ' + response.email);
+            });
+        } else {
+            console.log('User cancelled login or did not fully authorize.');
+        }
+    }, { scope: 'public_profile,email' });
+}
+
+// Github
+function signinWithGithub() {
+    const clientId = 'YOUR_GITHUB_CLIENT_ID';
+    const redirectUri = encodeURIComponent('http://yourwebsite.com/callback');
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user`;
+}
+
+// Email/Phone
+function signinWithEmail() {
+    const email = prompt("Vui lòng nhập email hoặc số điện thoại của bạn:");
+    if (email) {
+        console.log('Email/Phone:', email);
     }
-    else{
-        alert ("Đăng nhập thành công!");
-    }
 }
-
-function loginWithGoogle(){
-    alert("Đăng nhập bằng Google thành công!");
-}
-
-function loginWithFacebook(){
-    alert("Đăng nhập bằng Facebook thành công!");
-}
-
-function loginWithGithub(){
-    alert("Đăng nhập bằng Github thành công!");
-}
-
--- su&si/su.js
+++ su&si/su.js
@@ -1,23 +1,52 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
+// Google
+function signinWithGoogle() {
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signIn().then(googleUser => {
+        const profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId());
+        console.log('Name: ' + profile.getName());
+        console.log('Email: ' + profile.getEmail());
+    }).catch(error => {
+        console.error('Google Sign-In Error:', error);
+    });
+}
 
-    const username = document.getElementById('username').value.trim();
-    const password = document.getElementById('password').value.trim();
-    const terms = document.getElementById('terms').checked;
+// Facebook
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: 'YOUR_FACEBOOK_APP_ID',
+        cookie: true,
+        xfbml: true,
+        version: 'v12.0'
+    });
 
-    if (username === '') {
-        alert('Vui lòng nhập tên đăng nhập.');
-        return;
+    FB.AppEvents.logPageView();
+};
+
+function signinWithFacebook() {
+    FB.login(response => {
+        if (response.authResponse) {
+            FB.api('/me', { fields: 'name,email' }, function(response) {
+                console.log('Good to see you, ' + response.name + '.');
+                console.log('Email: ' + response.email);
+            });
+        } else {
+            console.log('User cancelled login or did not fully authorize.');
+        }
+    }, { scope: 'public_profile,email' });
+}
+
+// Github
+function signinWithGithub() {
+    const clientId = 'YOUR_GITHUB_CLIENT_ID';
+    const redirectUri = encodeURIComponent('http://yourwebsite.com/callback');
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user`;
+}
+
+// Email/Phone
+function signinWithEmail() {
+    const email = prompt("Vui lòng nhập email hoặc số điện thoại của bạn:");
+    if (email) {
+        console.log('Email/Phone:', email);
     }
-
-    if (password === '') {
-        alert('Vui lòng nhập mật khẩu.');
-        return;
-    }
-
-    if (!terms) {
-        alert('Vui lòng đồng ý với các điều khoản và điều kiện.');
-        return;
-    }
-    alert('Đăng ký thành công!');
-});
+}
